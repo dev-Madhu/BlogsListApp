4ca1acbd9ded4bf6ea8bcc49d2bada6b11ecc3f2
Madhu <mulaglam@gmail.com>
2022-05-15 05:18:01 +0000

    blogslist in home using react

---
+++ src/components/BlogItem/index.js
@@ -0,0 +1,20 @@
+// Write your JS code here
+import './index.css'
+
+const BlogItem = props => {
+  const {blogDetails} = props
+
+  const {title, description, publishedDate} = blogDetails
+  return (
+    <li className="blog-item">
+      <div className="title-container">
+        <h1 className="title">{title}</h1>
+        <p className="published-date">{publishedDate}</p>
+      </div>
+      <p className="description">{description}</p>
+      <hr className="line" />
+    </li>
+  )
+}
+
+export default BlogItem
---
+++ src/components/Home/index.js
@@ -0,0 +1,57 @@
+import BlogItem from '../BlogItem'
+import './index.css'
+
+const blogsList = [
+  {
+    id: 1,
+    title: 'My first post',
+    description: 'A high quality solution beautifully designed for startups',
+    publishedDate: 'Aug 2nd',
+  },
+  {
+    id: 2,
+    title: 'My second post',
+    description:
+      'A high quality solution beautifully designed for startups and Bussiness schools',
+    publishedDate: 'Mar 1st',
+  },
+  {
+    id: 3,
+    title: 'My third post',
+    description: 'A high quality solution beautifully designed for startups',
+    publishedDate: 'Jan 2nd',
+  },
+  {
+    id: 4,
+    title: 'My fourth post',
+    description:
+      'A high quality solution beautifully designed for startups and Bussiness schools. ',
+    publishedDate: 'Dec 24th',
+  },
+  {
+    id: 5,
+    title: 'My fifth post',
+    description: 'A high quality solution beautifully designed for startups',
+    publishedDate: 'Nov 10th',
+  },
+]
+const Home = () => (
+  <div className="home-container">
+    <div className="home-icon-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+        className="home-icon"
+        alt="profile"
+      />
+      <h1 className="profile-name">Wade Warren</h1>
+      <p className="role">Software developer at UK</p>
+    </div>
+    <ul className="blogItemsList">
+      {blogsList.map(eachBlog => (
+        <BlogItem blogDetails={eachBlog} key={eachBlog.id} />
+      ))}
+    </ul>
+  </div>
+)
+
+export default Home
